80ab7b4c6b8ef1ddfec584000895b60f277ceeb8
acbuitrago403 <ac.buitrago403@uniandes.edu.co>
2014-05-26 16:23:38 -0500

    Agregadas trayectorias

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,26 @@ class UsersController < ApplicationController
 		str="0"+str
 	end
 	@user=User.find_by_idN(str)
-	#puts @user._id.to_str
+	@lugares= @user.places
+	@trayectorias= @user.trajectories
+	@hash = Gmaps4rails.build_markers(@lugares) do |lugar, marker|
+		 marker.lat lugar.loc["lat"]
+		 marker.lng lugar.loc["lon"]
+		 marker.infowindow "Lugar visitado en #{lugar.diaSemana} <br /> #{lugar.avgHoraEntrada.strftime("%H:%M")} - #{lugar.avgHoraSalida.strftime("%H:%M")} <br /> #{lugar.avgHoraEntrada.strftime("%Y-%m-%d")}"
+	end
+	@trayectorias.each do |tray|
+                t=[]
+                muestras=tray.muestras
+                i=0
+                muestras.each do |m|
+                        pos={:lat=>m["lat"],:lng=>m["lon"]}
+                        if(i==0)
+                                pos["strokeColor"]="#FF0000"
+                                i=1
+                        end
+                        t.push(pos)
+                end
+                @polylines.push(t)
+        end
   end
 end
